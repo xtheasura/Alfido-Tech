48cf49de477744404bcbd46b91405213e9385187
Tarun Chauhan <90379712+xtheasura@users.noreply.github.com>
2023-11-16 20:30:17 +0530

    Add files via upload

---
+++ Word Counter/Tasktwo.java
@@ -0,0 +1,18 @@
+public class Tasktwo {
+
+    public static void main(String args[]) {
+
+        // Type a sentence with any count of words of your choice
+
+        String text = "text used for word counting";
+        String words[] = text.split("\\s");
+        int length = words.length;
+        System.out.println("Number Of words are:" + length);
+
+
+    }
+
+        
+
+    
+}
